e9bb85e6a1bde2a30317ca8493a15e7a28fc3814
RDKreisel <kreiselrd@gmail.com>
2019-03-21 18:32:49 -0500

    Added "political" branch

--- main.js
+++ main.js
@@ -7,57 +7,32 @@ function randomValueFromArray(array){
 }
 
 
-var storyText = 'It was :insertw: fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-var insertW = [105,74,-10];
-var insertX = ['an elephant','Father Christmas','Frosty the Snowman'];
-var insertY = ['the soup kitchen','Disneyland','the White House','the North Pole'];
-var insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+var storyText = 'It was :insertv:, so Donald Trump turned on the TV. When he saw the face of :insertx:, he :inserty:, then :insertz:. Bob read about it on the Washington Post website and thought, "Well, that was to be expected."';
+var insertV = ['11:00 p.m.',"2:00 a.m","11:00 a.m."];
+var insertX = ['John McCain','Hillary Clinton','Ronald McDonald','Robert Mueller'];
+var insertY = ['was enraged','smacked himself in the head','became excited','cried'];
+var insertZ = ['spontaneously combusted','melted into a puddle in the Oval Office','turned into a slug and crawled away','got out his cellphone and tweeted something insulting'];
 
 randomize.addEventListener('click',result);
 
 function result() {
 var newStory = storyText;
 
-var wItem = randomValueFromArray(insertW);
+var vItem = randomValueFromArray(insertV);
 var xItem = randomValueFromArray(insertX);
 var yItem = randomValueFromArray(insertY);
 var zItem = randomValueFromArray(insertZ);
 
-newStory = newStory.replace(':insertw:', wItem);
+newStory = newStory.replace(':insertv:', vItem);
 newStory = newStory.replace(':insertx:', xItem);
 newStory = newStory.replace(':insertx:', xItem);
 newStory = newStory.replace(':inserty:', yItem);
 newStory = newStory.replace(':insertz:', zItem);
 
-  if(customName.value !== '') {
+
+if(customName.value !== '') {
     var name = customName.value;
     newStory = newStory.replace('Bob',name);
-  }
-
-  if(wItem <= 25) {
-    newStory = newStory.replace('hot','bitterly cold');
-  }
-
-  if(wItem <= 0) {
-    newStory = newStory.replace(zItem,'froze into a huge block of ice');
-  }
-
- if (wItem >= 70 && wItem < 85 ){
- 	newStory = newStory.replace(zItem, 'enjoyed a nice dinner at a sidewalk cafe');
- }
-
- if (wItem >=70 && wItem < 85){
- 	newStory = newStory.replace('hot','pleasant');
- }
-
-if (xItem == 'an elephant'){
-  newStory = newStory.replace('300','12,000');
-}
-  if(document.getElementById('uk').checked) {
-    var weight = Math.round(300/14) + ' stone';
-    var temperature =  Math.round((94-32) * 5 / 9) + ' degrees centigrade';
-	newStory = newStory.replace('300 pounds',weight);
-	newStory = newStory.replace('94 fahrenheit',temperature);
   }
 
   story.textContent = newStory;
